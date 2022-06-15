7e3fa595ae5930d56301abe1bce3e841c669bf87
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-15 13:15:08 +0200

    fix buttons

--- src/components/Commons/HeaderCommon/WelcomeHeader.vue
+++ src/components/Commons/HeaderCommon/WelcomeHeader.vue
@@ -3,8 +3,8 @@
     <div class="py-60 text-center "  >
          <h1 class="text-5xl font-bold">MAKE A DIFFERENCE</h1>
          <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsam unde</p>
-         <button class="bt-white py-2 px-2 mt-3">Our Mission</button>
-         <button class="mx-2 bt-orange py-2 px-2 mt-3">Donate Now</button>
+         <button class="bt-white py-0.5 px-3 mt-4">Our Mission</button>
+         <button class="mx-2 bt-orange py-0.5 px-4 mt-3">Donate Now</button>
     </div>
     
   </div>
