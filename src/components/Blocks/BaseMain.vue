090813ceeef2f10791b9164c511eb7ccef9128c7
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-15 18:15:56 +0200

    add scection2main

--- src/components/Blocks/BaseMain.vue
+++ src/components/Blocks/BaseMain.vue
@@ -1,14 +1,18 @@
 <template>
   <main>
      <Section1MainVue/>
+     <Section2MainVue/>
   </main>
 </template>
 
 <script>
 import Section1MainVue from '../Commons/MainCommons/Section1Main.vue';
+import Section2MainVue from '../Commons/MainCommons/Section2Main.vue';
+
+
 export default {
     name: "BaseMain",
-    components: { Section1MainVue }
+    components: { Section1MainVue, Section2MainVue }
 }
 </script>
 
--- src/components/Commons/MainCommons/Section2Main.vue
+++ src/components/Commons/MainCommons/Section2Main.vue
@@ -1,15 +1,90 @@
 <template>
-  <div>
-
+  <div class="background">
+        <div class="w-full flex justify-center items-center pt-44  text-white  ">
+          <div>
+             <h2 class="font-bold text-2xl mb-6" > OUR VALUES </h2>
+             <hr class="w-38 border-yellow-600  mb-6">
+             <h5> Think Big.Act Bigger.</h5>
+          </div>
+         
+        </div>
+        <div class="w-full flex text-center">
+            <div class="w-1/4  text-white py-40 px-10 " >
+               <div>
+                  <span class=""><i class="fa-solid fa-droplet text-orange-300 text-3xl "></i></span>
+               </div>
+               <div class="pb-6">
+                  <h5>Clean Water</h5>
+               </div>
+               <div class="font-extralight pb-5">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex </p>
+               </div>
+               <div class=" flex py-2 px-4 w-32 cursor-pointer bg-amber-500 text-white text-center ml-20">
+                  <button class="text-sm ml-1">LEARN MORE</button>
+               </div>
+            </div>
+            <div class="w-1/4  text-white py-40 px-10 " >
+               <div>
+                  <span class=""><i class="fa-solid fa-droplet text-orange-300 text-3xl "></i></span>
+               </div>
+               <div class="pb-6">
+                  <h5>Clean Water</h5>
+               </div>
+               <div class="font-extralight pb-5">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex </p>
+               </div>
+               <div class=" flex py-2 px-4 w-32 cursor-pointer bg-amber-500 text-white text-center ml-20">
+                  <button class="text-sm ml-1">LEARN MORE</button>
+               </div>
+            </div>
+            <div class="w-1/4  text-white py-40 px-10 " >
+               <div>
+                  <span class=""><i class="fa-solid fa-droplet text-orange-300 text-3xl "></i></span>
+               </div>
+               <div class="pb-6">
+                  <h5>Clean Water</h5>
+               </div>
+               <div class="font-extralight pb-5">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex </p>
+               </div>
+               <div class=" flex py-2 px-4 w-32 cursor-pointer bg-amber-500 text-white text-center ml-20">
+                  <button class="text-sm ml-1">LEARN MORE</button>
+               </div>
+            </div>
+            <div class="w-1/4  text-white py-40 px-10 " >
+               <div>
+                  <span class=""><i class="fa-solid fa-droplet text-orange-300 text-3xl "></i></span>
+               </div>
+               <div class="pb-6">
+                  <h5>Clean Water</h5>
+               </div>
+               <div class="font-extralight pb-5">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex </p>
+               </div>
+               <div class=" flex py-2 px-4 w-32 cursor-pointer bg-amber-500 text-white text-center ml-20">
+                  <button class="text-sm ml-1">LEARN MORE</button>
+               </div>
+            </div>
+           
+       
+       
+        </div> 
+        
+       
   </div>
+   
 </template>
 
 <script>
 export default {
- name: 'Section2Manu',
+ name: 'Section2Main',
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .background{
+   background-image: url(../../../assets/images/home-content-bg-2.jpg);
+   background-size: contain;
+   background-size: cover;
+  }
 </style>
