434b47d4213e58e1dcd3215b7f2604422d13318b
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-15 12:44:48 +0200

    staticst NavHeader fatto

--- src/components/Blocks/BaseHeader.vue
+++ src/components/Blocks/BaseHeader.vue
@@ -1,8 +1,8 @@
 <template>
    <header>
       <div class="background">
-        <h1>Some text</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat dolor fugiat nostrum quibusdam eius molestiae rerum laboriosam voluptatum magnam quis alias asperiores totam animi perferendis fuga id officia, cupiditate quod.</p>
+        <!-- <h1>Some text</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat dolor fugiat nostrum quibusdam eius molestiae rerum laboriosam voluptatum magnam quis alias asperiores totam animi perferendis fuga id officia, cupiditate quod.</p> -->
         <NavbarHeaderVue/>
         <WelcomeHeaderVue/>
       </div>
--- src/components/Commons/HeaderCommon/NavbarHeader.vue
+++ src/components/Commons/HeaderCommon/NavbarHeader.vue
@@ -1,6 +1,17 @@
 <template>
-  <nav>
-
+  <nav class="w-full flex justify-between p-4 items-center">
+    <div class=" flex justify-start items-center">
+       <img src="../../../assets/images/avada-charity-logo.png" alt="" class="w-37 cursor-pointer">
+    </div>
+    <ul class="flex list-none flex-row justify-between items-center flex-initial  ">
+      <li><a href="#"></a>Home</li>
+      <li><a href="#"></a>Mission</li>
+      <li><a href="#"></a>Causes</li>
+      <li><a href="#"></a>Jurnal</li>
+       <li class=' py-2 px-7 mx-4  cursor-pointer bg-amber-500 '>
+            login
+       </li>
+    </ul>
   </nav>
 </template>
 
@@ -11,5 +22,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ li{
+  margin: 0 20px;
+ }
 
 </style>
