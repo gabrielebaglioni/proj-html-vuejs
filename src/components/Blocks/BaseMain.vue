e0117c3a7a9ecb4a49b071c431a5862753192683
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-15 19:15:44 +0200

    Separator Done

--- src/components/Blocks/BaseMain.vue
+++ src/components/Blocks/BaseMain.vue
@@ -3,6 +3,7 @@
      <Section1MainVue/>
      <Section2MainVue/>
      <Section3MainVue/>
+     <SeparatorMainVue/>
   </main>
 </template>
 
@@ -10,11 +11,14 @@
 import Section1MainVue from '../Commons/MainCommons/Section1Main.vue';
 import Section2MainVue from '../Commons/MainCommons/Section2Main.vue';
 import Section3MainVue from '../Commons/MainCommons/Section3Main.vue';
+import SeparatorMainVue from '../Commons/MainCommons/SeparatorMain.vue';
+
+
 
 
 export default {
     name: "BaseMain",
-    components: { Section1MainVue, Section2MainVue, Section3MainVue }
+    components: { Section1MainVue, Section2MainVue, Section3MainVue, SeparatorMainVue }
 }
 </script>
 
---
+++ src/components/Commons/MainCommons/SeparatorMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full text-center py-10 background text-white font-light">
+   <h4>VIEW ALL OUR CAUSES</h4>
+  </div>
+</template>
+
+<script>
+export default {
+ name:'SeparatorMain'
+}
+</script>
+
+<style lang="scss" scoped>
+ .background{
+  background-color: #ED6966;
+ }
+</style>
