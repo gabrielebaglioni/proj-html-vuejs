f25047ba222f07eb6c8b014163a3258da2096007
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-15 18:35:03 +0200

    section3Main done

--- src/components/Blocks/BaseMain.vue
+++ src/components/Blocks/BaseMain.vue
@@ -2,17 +2,19 @@
   <main>
      <Section1MainVue/>
      <Section2MainVue/>
+     <Section3MainVue/>
   </main>
 </template>
 
 <script>
 import Section1MainVue from '../Commons/MainCommons/Section1Main.vue';
 import Section2MainVue from '../Commons/MainCommons/Section2Main.vue';
+import Section3MainVue from '../Commons/MainCommons/Section3Main.vue';
 
 
 export default {
     name: "BaseMain",
-    components: { Section1MainVue, Section2MainVue }
+    components: { Section1MainVue, Section2MainVue, Section3MainVue }
 }
 </script>
 
---
+++ src/components/Commons/MainCommons/Section3Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+         <div class="w-full flex justify-center items-center pt-20 text-center ">
+               <div>
+                  <h2 class="font-bold text-2xl mb-6" > RECENT CAUSES </h2>
+                  <hr class="w-96 border-yellow-600  mb-6">
+                  <h5> We run project in over 30 countries in 5 contines</h5>
+               </div>
+               
+         </div>
+         <div class="w-full flex py-20 items-center justify-center">
+            <div class="w-1/5 ml-6">
+               <img src="../../../assets/images/avada-charity-fair-trade-featured-400x300.jpg" alt="">
+            </div>
+            <div class="w-1/5 ml-6">
+               <img src="../../../assets/images/avada-charity-shelter-featured-400x300.jpg" alt="">
+            </div>
+            <div class="w-1/5 ml-6">
+               <img src="../../../assets/images/avada-charity-fair-trade-featured-400x300.jpg" alt="">
+            </div>
+            <div class="w-1/5 ml-6">
+               <img src="../../../assets/images//avada-charity-vaccines-featured-400x300.jpg" alt="">
+            </div>
+            
+
+             
+         </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name:'Section3Main'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
